ebe59181264251323e542bba62858e6c65c3e87d
kianamcc <42532725+kianamcc@users.noreply.github.com>
2018-08-28 17:18:17 -0700

    ball works

--- Pong/kmccu003_custom_project/kmccu003_custom_project/main.c
+++ Pong/kmccu003_custom_project/kmccu003_custom_project/main.c
@@ -16,7 +16,12 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 unsigned char button = 0x00;
 unsigned char play_flag = 0;
-//unsigned char count = 0;
+unsigned char p1_score = 0;
+unsigned char p2_score = 0;
+unsigned char disp_flag = 0;
+unsigned char win = 0;
+unsigned char button_flag = 0;
+
 enum Menu {init, play, matrix} menu;
 
 void Menu() {
@@ -37,6 +42,11 @@ void Menu() {
 		  
 		case play:
 			menu = play;
+			/*else if(win == 1) {
+				win = 0; //reset
+				menu = init;
+			}*/
+			
 			break;
 			
 		case matrix:
@@ -60,8 +70,33 @@ void Menu() {
 		 break;
 			
 		case play:
+		
+			if(disp_flag == 0) {
 			LCD_ClearScreen();
 			LCD_DisplayString(1, "     SCORE:     P1: 0      P2: 0");
+			disp_flag = 1;
+			}
+			
+			LCD_Cursor(21);
+			LCD_WriteData(p1_score +'0');
+			LCD_Cursor(32);
+			LCD_WriteData(p2_score +'0');
+			
+			if (p1_score == 7) {
+			LCD_ClearScreen();
+			LCD_DisplayString(1, "PLAYER 1 WINS!");
+			win = 1;
+			//matrix_clear();
+			//disp_flag = 0;
+			}
+			
+			if (p2_score == 7) {
+				LCD_ClearScreen();
+				LCD_DisplayString(1, "PLAYER 2 WINS!");
+				win = 1;
+				//matrix_clear();
+				//disp_flag = 0;
+			}
 			break;
 		   
 		  default:
@@ -76,12 +111,16 @@ struct p1_struct { //type of struct
 	
 	unsigned char y; //coordinates
 	unsigned char x;
+	unsigned char xr; //right x coordinate
+	unsigned char xl; //left x coordinate
 	
 } p1;
 
 void p1_init() {
 	p1.y = 1;
 	p1.x = 28;
+	p1.xr = 16; //multiply by 2 each time paddle moves right and divide by two when it moves left
+	p1.xl = 4;
 }
 
 unsigned char button_right = 0x00;
@@ -194,8 +233,10 @@ void P1() {
 		
 		case right_press:
 
-			if(p1.x < 224) {
+			if(p1.x < 224 && p1.xr < 128) {
 				p1.x  = p1.x * 2;
+				p1.xr = p1.xr * 2; //individual bit, NEW
+				p1.xl = p1.xl * 2;
 			}
 			
 			//matrix_write(p1.y, p1.x);
@@ -205,6 +246,8 @@ void P1() {
 
 			if(p1.x != 7) {
 				p1.x = p1.x / 2;
+				p1.xr = p1.xr / 2; //individual bit, NEW
+				p1.xl = p1.xl / 2;
 			}
 			
 			//matrix_write(p1.y, p1.x);
@@ -215,9 +258,12 @@ void P1() {
 		
 		case reset: 
 			matrix_clear();
+			LCD_ClearScreen();
+			disp_flag = 0;
+			p1_score = 0;
+			p2_score = 0;
 			count_right = 28;
-			//matrix_write(1, 28); //p1 paddle in middle
-			//matrix_write(8, 28); //p2 paddle in middle
+			
 			break;	
 	}	
 } //end player 1
@@ -227,14 +273,17 @@ struct p2_struct { //type of struct
 	
 	unsigned char y2; //coordinates
 	unsigned char x2;
+	unsigned char xr2;
+	unsigned char xl2;
 	
 } p2;
 
 void p2_init() {
 	p2.y2 = 8;
 	p2.x2 = 28;
+	p2.xr2 = 16; //multiply by 2 each time paddle moves right and divide by two when it moves left
+	p2.xl2 = 4;
 }
-
 
 unsigned char button_right_2 = 0x00;
 unsigned char button_left_2 = 0x00;
@@ -347,6 +396,8 @@ void P2() {
 
 			if(p2.x2 < 224) {
 				p2.x2  = p2.x2 * 2;
+				p2.xr2 = p2.xr2 * 2; //individual bit, NEW
+				p2.xl2 = p2.xl2 * 2;
 			}
 	
 			//matrix_write(p1.y, p1.x);
@@ -356,6 +407,8 @@ void P2() {
 		
 			if(p2.x2 != 7) {
 				p2.x2 = p2.x2 / 2;
+				p2.xr2 = p2.xr2 / 2; //individual bit, NEW
+				p2.xl2 = p2.xl2 / 2;
 			}
 			
 			//matrix_write(p1.y, p1.x);
@@ -366,10 +419,11 @@ void P2() {
 		
 		case reset_p2:
 			matrix_clear();
+			LCD_ClearScreen();
+			disp_flag = 0;
+			p1_score = 0;
+			p2_score = 0;
 			count_right = 28;
-			//matrix_write(1, 28); //p1 paddle in middle
-			//matrix_write(8, 28); //p2 paddle in middle
-			break;
 	}
 } //end player 2
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -388,9 +442,11 @@ struct ball_struct { //type of struct
 
 void ball_init() {
 	
+	//matrix_write(ball.x, 0);
 	ball.direction = 0;
 	ball.y = 5;
 	ball.x = 8;
+	matrix_write(ball.y, ball.x); //p2 paddle in middle
 }
 
 unsigned char p1_ball_check() {
@@ -400,14 +456,11 @@ unsigned char p1_ball_check() {
 unsigned char p2_ball_check() {
 	return ball.x & p2.x2;
 }
-//unsigned char p2_ball_check() {
-	//return ball.x & p2.x;
-//}
-//unsigned char ball_count = 5; //start at 4, 8
+
 unsigned char ball_column_index = 5;
 unsigned char ball_row_index = 8;
 
-enum ball_movement{init_ball, ball_start, ball_vertical/*, ball_tr, ball_tl, ball_br, ball_bl*/} ball_state; //top right, bottom right, etc...
+enum ball_movement{init_ball, ball_start, ball_vertical/*, ball_p1, ball_2*/} ball_state; //top right, bottom right, etc...
 
 void Ball() {
 	
@@ -427,13 +480,16 @@ void Ball() {
 			
 		case ball_start:
 			if (ball.y <= 2) { //ball near p1
-			ball_state = ball_vertical;
+				ball_state = ball_vertical;
 			}
+			
 			if (ball.y >= 7)
 			{
 				ball_state = ball_vertical;
 			}
-			//ball_state = ball_start;
+			if(p1_score == 7 || p2_score == 7) {
+				ball_state = init_ball;
+			}
 			break;
 			
 		case ball_vertical:
@@ -449,13 +505,62 @@ void Ball() {
 			break;
 			
 		case ball_start: //ball go down towards p1
+		
 			matrix_write(ball.y, 0);
-			if (ball.direction == 0) { //stops right in front of paddle
+			matrix_write(ball.x, 0);
+			
+			if (ball.direction == 0) { //move down from p2
 				ball.y -= 1;
 			}
-			if (ball.direction == 1)
+			if (ball.direction == 1) //move up from p1
 			{
 				ball.y += 1;
+			}
+			if (ball.direction == 2) //move right from p1
+			{
+				if(ball.x < 128) {
+					ball.y += 1;
+					ball.x *= 2;
+				}
+				else { 
+					ball.direction = 3;
+					break;
+				}
+			}
+			if (ball.direction == 3) //move left from p1
+			{
+				if(ball.x > 1) {
+					ball.y += 1;
+					ball.x /= 2;
+				}
+				else {
+					ball.direction = 2;
+					break;
+				}
+			}
+			
+			if (ball.direction == 4) { //right from p2
+				if(ball.x < 128) {
+					ball.y -= 1;
+					ball.x *= 2;
+				}
+				else { 
+					ball.direction = 5;
+					break;
+				}
+			}
+			
+			if (ball.direction == 5) //move left from p2
+			{
+				if(ball.x > 1) {
+					ball.y -= 1;
+					ball.x /= 2;
+				}
+				
+				else {
+					ball.direction = 4;
+					break;
+				}
 			}
 			matrix_write(ball.y, ball.x);
 			break;
@@ -464,27 +569,67 @@ void Ball() {
 				if(ball.y <= 2) { //check player coordinate,
 					if (p1_ball_check())
 					{
-						ball.direction = 1;
+						if(p1.xr != ball.x && p1.xl != ball.x) { //if not rightmost bit and leftmost bit
+							ball.direction = 1;
+						}
+						
+						else if(p1.xr == ball.x) {
+							if(p1.xr != 128) {
+								ball.direction = 2; //right up
+							}
+							else if (p1.xr == 128) {
+								ball.direction = 3;
+							}
+						}
+							
+						else if(p1.xl == ball.x) //left up
+						{
+							ball.direction = 3;
+						}
 					}
-					else if(ball.y == 1)
+					else if(ball.y == 1) //if it reaches ground
 					{
 						matrix_write(ball.y, 0);
+						p2_score++;
+						//score(p1_score, p2_score);
 						ball_init();
 					}
-				}
-				if (ball.y >= 7)
+				} //end p1 check
+				
+				if (ball.y >= 7) //if near p2
 				{
-					if(p2_ball_check()) 
+					if(p2_ball_check()) //if p2 hits it
 					{
-						ball.direction = 0;
+						if (p2.xr2 != ball.x && p2.xl2 != ball.x) { //middle
+							ball.direction = 0; //down
+						}
+					
+						else if(p2.xr2 == ball.x) { //right side of paddle, not left
+							if(p2.xr2 != 128) {
+								ball.direction = 4; //right from p2
+							}
+							else if(p2.xr2 == 128) {
+								ball.direction = 5;
+							}
+						}
+						
+						else if(p2.xl2 == ball.x) //left from p2
+						{
+							ball.direction = 5;
+						}
 					}
-					else if(ball.y == 8)
+					
+					else if(ball.y == 8) //reaches p2 ground
 					{
 						matrix_write(ball.y, 0);
+						p1_score++;
+						//score(p1_score, p2_score);
 						ball_init();
 					}
 				}
 				break;
+				
+
 		
 	}	
 	
@@ -527,13 +672,13 @@ int main(void)
 	DDRA = 0xFF; PORTA = 0x00; //lcd
 	DDRD = 0xFF; PORTD = 0x00; //lcd
 	
-	unsigned long ball_timer = 100;
+	//unsigned long ball_timer = 150;
 	
 	menu = init;
 	
 	matrix_init();
 	
-	TimerSet(100);
+	TimerSet(150);
 	TimerOn();
 	
    // Initializes the LCD display
@@ -548,11 +693,11 @@ int main(void)
 	   
 	   P2();
 	   
-	   if(ball_timer == 300) {
+	   //if(ball_timer == 300) {
 		   Ball();
-		   ball_timer = 0;
-	   }
-	   ball_timer += 100;
+		  // ball_timer = 0;
+	   //}
+	  // ball_timer += 150;
 	   
 	   display();
 	   
